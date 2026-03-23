09eaca9cb08cc64ad050ec6f6e8493549ea4125a
TonNomGitHub <excellentservice1exls@gmail.com>
2026-03-23 14:37:10 +0100

    mise à jour de "lib/utils.ts"

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,22 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+/**
+ * Permet de fusionner des classes Tailwind proprement, 
+ * en gérant les conflits et les conditions.
+ */
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+/**
+ * Formate un nombre en devise Dollar Américain (USD).
+ */
+export const formatPrice = (price: number) => {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(price);
+};
