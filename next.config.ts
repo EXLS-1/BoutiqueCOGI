34c916e4fe44292ae0a2752661f66691a212b19b
TonNomGitHub <excellentservice1exls@gmail.com>
2026-03-27 20:56:11 +0100

    modifs de next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Configuration pour les images distantes
+  // Configuration pour les images distantes (Stripe)
   images: {
     remotePatterns: [
       {
@@ -12,7 +12,7 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  // Autoriser les origines en développement (pour l'erreur cross-origin)
+  // Autoriser les origines en développement (évite l'avertissement cross-origin)
   allowedDevOrigins: ['10.47.93.188', 'localhost', '127.0.0.1'],
 };
 
