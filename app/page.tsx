f280c444f991061875136acd1100104a8170b33e
TonNomGitHub <excellentservice1exls@gmail.com>
2026-03-28 22:20:23 +0100

    mise à jour

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,22 @@
 // app/page.tsx
-"use client";
 
-import { useState } from "react";
 import Hero from "@/components/hero";
 import Boutique from "@/components/boutique";
-import ProductCatalog from "@/components/product-catalog";
-import Contact from "@/components/contact";
-import SocialNetworks from "@/components/social-networks";
-import Footer from "@/components/footer";
 import { ProductList } from "@/components/product-list";
-import { getAllProducts } from "@/lib/products.ts";
+import { getAllProducts } from "@/lib/products";
 
 export default function Home() {
-  const allProducts = getAllProducts();
+  const products = getAllProducts();
+
   return (
-    <div>
+    <main>
       <Hero />
       <Boutique />
-      <ProductList products={allProducts} title="Nos dernières nouveautés" />
-      {/* ... autres sections */}
-    </div>
+      <ProductList
+        title="Nos dernières nouveautés"
+        products={products}
+      />
+      {/* autres sections */}
+    </main>
   );
 }
