a390faa6f6a192b2c03386247986646e36e6b399
TonNomGitHub <excellentservice1exls@gmail.com>
2026-03-28 07:09:12 +0100

    modifs

--- app/page.tsx
+++ app/page.tsx
@@ -11,17 +11,17 @@ export default function Home() {
         <div className="absolute inset-0 w-full h-full flex">
           <div className="w-1/2 h-full relative">
             <video className="absolute inset-0 w-full h-full object-cover opacity-60" autoPlay muted loop playsInline poster="/video-poster1.jpg">
-              <source src="public/Media-v-20260218/Vids20260212at0710PM.mp4" type="video/mp4" />
+              <source src="/Media-v-20260218/Vids20260212at0710PM.mp4" type="video/mp4" />
             </video>
           </div>
           <div className="w-1/2 h-full relative">
             <video className="absolute inset-0 w-full h-full object-cover opacity-60" autoPlay muted loop playsInline poster="/video-poster2.jpg">
-              <source src="public/Media-v-20260218/Vids20260212at0710PM.mp4" type="video/mp4" />
+              <source src="/Media-v-20260218/Vids20260212at0710PM.mp4" type="video/mp4" />
             </video>
           </div>
           <div className="w-1/2 h-full relative">
             <video className="absolute inset-0 w-full h-full object-cover opacity-60" autoPlay muted loop playsInline poster="/video-poster2.jpg">
-              <source src="public/Media-v-20260218/WhatsApp Video 2026-02-18 at 07.10.31 PM.mp4" type="video/mp4" />
+              <source src="/Media-v-20260218/WhatsApp Video 2026-02-18 at 07.10.31 PM.mp4" type="video/mp4" />
             </video>
           </div>
         </div>
@@ -67,7 +67,7 @@ export default function Home() {
             
             {/* Catégorie : Femme */}
             <div className="group relative aspect-4/5 overflow-hidden rounded-2xl bg-gray-200 shadow-md">
-              <img src="public/Media-p-20260218/pict01.jpeg" alt="Habit Femme" className="w-full h-full object-cover transition-transform duration-700 group-hover:scale-110" />
+              <img src="/Media-p-20260218/pict01.jpeg" alt="Habit Femme" className="w-full h-full object-cover transition-transform duration-700 group-hover:scale-110" />
               <div className="absolute inset-0 bg-linear-to-t from-black/80 via-black/20 to-transparent flex flex-col justify-end p-8 text-white">
                 <h3 className="font-playfair text-2xl font-bold mb-2">HABIT FEMME</h3>
                 <p className="text-gray-200 mb-6">Élégance et sophistication</p>
@@ -77,7 +77,7 @@ export default function Home() {
 
             {/* Catégorie : Homme */}
             <div className="group relative aspect-4/5 overflow-hidden rounded-2xl bg-gray-200 shadow-md">
-              <img src="/category-homme.jpg" alt="Habit Homme" className="w-full h-full object-cover transition-transform duration-700 group-hover:scale-110" />
+              <img src="/category-homme.jpeg" alt="Habit Homme" className="w-full h-full object-cover transition-transform duration-700 group-hover:scale-110" />
               <div className="absolute inset-0 bg-linear-to-t from-black/80 via-black/20 to-transparent flex flex-col justify-end p-8 text-white">
                 <h3 className="font-playfair text-2xl font-bold mb-2">HABIT HOMME</h3>
                 <p className="text-gray-200 mb-6">Style moderne et raffiné</p>
