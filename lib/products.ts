1981c58424913bb437d171c51d8c2e975397d77c
TonNomGitHub <excellentservice1exls@gmail.com>
2026-03-28 21:54:00 +0100

    mise à jour

--- lib/products.ts
+++ lib/products.ts
@@ -1,10 +1,14 @@
-// lib/products.ts
 import rawData from "@/data/product-data.json";
+import { Product } from "@/types/product";
 
-export const getAllProducts = () => {
-  const all = [];
-  for (const cat of Object.keys(rawData.products)) {
-    all.push(...rawData.products[cat]);
-  }
-  return all;
+export const getAllProducts = (): Product[] => {
+  return Object.values(rawData.products)
+    .flat()
+    .map((product: any) => ({
+      ...product,
+      price: Number(product.price),
+      image: product.image.startsWith("/")
+        ? product.image
+        : `/${product.image}`,
+    }));
 };
