227f1f70b9cbd664a8e319d484f1e0e5eeaf1028
TonNomGitHub <excellentservice1exls@gmail.com>
2026-03-29 18:01:43 +0100

    mise à jour

--- lib/products.ts
+++ lib/products.ts
@@ -1,17 +1,15 @@
 import rawData from "@/data/product-data.json";
-import { Product } from "@/types/product";
+import { Product } from "@/types/products";
 import { cache } from "react";
 
-export const getAllProducts = cache(async (): Promise<Product[]> => {
-  const products = Object.values(rawData.products)
-    .flat()
-    .map((p: any) => ({
-      ...p,
-      price: Number(p.price),
-      image: p.image.startsWith("/")
-        ? p.image
-        : `/${p.image}`,
-    }));
-
-  return products;
-});
+export const getAllProducts = cache(
+  async (): Promise<Product[]> => {
+    return Object.values(rawData.products)
+      .flat()
+      .map((p: any) => ({
+        ...p,
+        price: Number(p.price),
+        image: p.image.startsWith("/") ? p.image : `/${p.image}`,
+      }));
+  }
+);
